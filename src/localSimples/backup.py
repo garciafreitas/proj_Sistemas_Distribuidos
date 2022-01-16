0dddb924dd752de955c05d11056da3e5e3410a69
garciafreitas <gthiago_freitas@hotmail.com>
2022-01-16 16:32:58 -0300

    atualização codigo localSimples

--- src/localSimples/backup.py
+++ src/localSimples/backup.py
@@ -1,47 +1,46 @@
-#local simples não possui qualquer RPC, apenas a programação tradicional, tudo em um unico bloco, necessário utilizar paralelismo de threads
-#enquanto uma thread irá processar (multiplicar) uma parte da matriz enquanto que outra thread processa (multiplica) a outra parte.
-#Ao final, você precisará realizar a junção dos dois resultados (merging) para compor um resultado final
-#https://pt.stackoverflow.com/questions/384972/como-fa%C3%A7o-para-multiplicar-duas-matrizes-no-python
+import time
 
-#def main()
-def main():  
-    #Carregar os arq txt das matrizes
+createMatrizC = open('matC.txt', 'w')
+matrizResultado = []
 
-    #loadMatrizA recebe o  comando open responsável por localizar e trazer o arq
-    #loadedMatrizA recebe a var anterior e realiza a leitura das linhas
-    #no print é mostrado a Matriz A, processo se repete para a B
-    
-    loadMatrizA = open("./matrizes_win/matA_win_teste.txt",'r')
-    loadedMatrizA =loadMatrizA.readlines()#matriz A carregada
-    print("Matriz A:\n\t",loadedMatrizA)
-    
-    #carregar matriz B
-    loadMatrizB = open("./matrizes_win/matB_win_teste.txt",'r')
-    loadedMatrizB =loadMatrizB.readlines()#matriz B carregada
-    print("Matriz B:\n\t",loadedMatrizB)
+#carregar matriz A
+loadMatrizA = open("./matrizes_win/matA_win_teste.txt",'r')
+loadedMatrizA = loadMatrizA.readlines()#matriz A carregada
+map(float, loadedMatrizA)
+print("Matriz A:\n\t",loadedMatrizA)
 
-    class multiMatrizes():
-        def __init__(self,mat):
-            self.mat = mat
-            self.lin = len(loadedMatrizA)
-            self.col= len(loadedMatrizB)
+#carregar matriz B
+loadMatrizB = open("./matrizes_win/matB_win_teste.txt",'r')
+loadedMatrizB = loadMatrizB.readlines()#matriz B carregada
+map(float, loadedMatrizB)
+print("Matriz B:\n\t",loadedMatrizB)
+
+#print ("\tMatrizResultado --> \n",loadedMatrizA * loadMatrizB)
+
+def multiMatrizes():
+    def _init_(self,loadedMatrizA):
+        self.mat = loadedMatrizA
+        self.linha_A = len(loadedMatrizA.linha_A[0])
+        self.coluna_A= len(loadedMatrizA.coluna_A[0])
         
-        def getLinha(self, nLinhas):
-            return[i for i in self.mat[nLinhas]]
-        
-        def getColunas(self, nColunas):
-            return[i[nColunas] for i in self.mat]
-        
-        def __mul__(self,mat2):
-            matrizResultado = []
-            
-
-            for i in range(self.lin):
-                matrizResultado.append([])
-
-                for j in range(mat2.col):
-                    listMult = [x*y for x, y in open(self.getLinha(i), mat2.getColunas(j))]
-                    matrizResultado[i].append(sum(listMult))
-    print("Resultado: \n",loadedMatrizA * loadedMatrizB)
+    def _mul_(self,loadedMatrizB):
+        self.mat = loadedMatrizB
+        self.mat = loadedMatrizB
+        self.linhaha_B = len(loadedMatrizB.linha_B[0])
+        self.coluna_B= len(loadedMatrizB.coluna_B[0])
+    assert loadedMatrizA.coluna_A == loadedMatrizB.coluna_B
 
     
+    for linha_A in range(loadedMatrizA.linha_A):
+            # Adicionando linha
+                matrizResultado = []
+                for colunauna in range(loadedMatrizB.coluna_B):
+                        #adicionando uma nova coluna na linha
+                        matrizResultado[loadedMatrizA.linha_A].append(0)
+                        for k in range(loadedMatrizA.coluna_A):
+                            matrizResultado[loadedMatrizA.linha_A][colunauna] += loadedMatrizA.coluna_A[loadedMatrizA.linha_A][k] * loadedMatrizB.coluna_B[k][colunauna]
+    return matrizResultado
+createMatrizC.readlines(matrizResultado)
+print('Resultado:',matrizResultado)
+print("\n\nMatriz C foi criada com o calculo.")
+time.perf_counter()
--- src/localSimples/localSimples.py
+++ src/localSimples/localSimples.py
@@ -1,42 +1,46 @@
 import time
 
 createMatrizC = open('matC.txt', 'w')
-
-loadMatrizA = open("./matrizes_win/matA_win_teste.txt",'r')
+matrizResultado = []
 
 #carregar matriz A
+loadMatrizA = open("./matrizes_win/matA_win_teste.txt",'r')
 loadedMatrizA = loadMatrizA.readlines()#matriz A carregada
+map(float, loadedMatrizA)
 print("Matriz A:\n\t",loadedMatrizA)
 
 #carregar matriz B
 loadMatrizB = open("./matrizes_win/matB_win_teste.txt",'r')
 loadedMatrizB = loadMatrizB.readlines()#matriz B carregada
+map(float, loadedMatrizB)
 print("Matriz B:\n\t",loadedMatrizB)
 
-print ("\tMatrizResultado --> \n",loadedMatrizA * loadMatrizB)
+#print ("\tMatrizResultado --> \n",loadedMatrizA * loadMatrizB)
 
 def multiMatrizes():
     def _init_(self,loadedMatrizA):
-        self.mat = mat
-        self.linha_A = len(linha_A[0])
-        self.coluna_A= len(coluna_A[0])
+        self.mat = loadedMatrizA
+        self.linha_A = len(loadedMatrizA.linha_A[0])
+        self.coluna_A= len(loadedMatrizA.coluna_A[0])
         
     def _mul_(self,loadedMatrizB):
-        self.mat = mat
-        self.linhaha_B = len(linha_B[0])
-        self.coluna_B= len(coluna_B[0])
-    assert coluna_A == coluna_B
+        self.mat = loadedMatrizB
+        self.mat = loadedMatrizB
+        self.linhaha_B = len(loadedMatrizB.linha_B[0])
+        self.coluna_B= len(loadedMatrizB.coluna_B[0])
+    assert loadedMatrizA.coluna_A == loadedMatrizB.coluna_B
 
-    matrizResultado = []
-    for linhaha in range(linha_A):
+    
+    for linha_A in range(loadedMatrizA.linha_A):
             # Adicionando linha
                 matrizResultado = []
-                for colunauna in range(coluna_B):
+                for colunauna in range(loadedMatrizB.coluna_B):
                         #adicionando uma nova coluna na linha
-                        matrizResultado[linhaha].append(0)
-                        for k in range(coluna_A):
-                            matrizResultado[linhaha][colunauna] += coluna_A[linhaha][k] * coluna_B[k][colunauna]
+                        matrizResultado[loadedMatrizA.linha_A].append(0)
+                        for k in range(loadedMatrizA.coluna_A):
+                            matrizResultado[loadedMatrizA.linha_A][colunauna] += loadedMatrizA.coluna_A[loadedMatrizA.linha_A][k] * loadedMatrizB.coluna_B[k][colunauna]
     return matrizResultado
-createMatrizC.writelinhaes(matrizResultado)
-print("Matriz C foi criada com o calculo.")
+createMatrizC.readlines(matrizResultado)
+print('Resultado:',matrizResultado)
+print("\n\nMatriz C foi criada com o calculo.")
 time.perf_counter()
